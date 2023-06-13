c28b488bf80ed7a72fac2837e50a0fed59c1a7d7
maromaya <116757556+maromaya@users.noreply.github.com>
2023-06-13 19:27:19 +0300

    Add files via upload

---
+++ 209162544_207004078_assignment2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, Extension
+
+"""
+A minimalist setup is shown.
+"""
+
+
+setup(name='mykmeanssp',
+      version='1.0',
+      description='kmeans in c',
+      ext_modules=[Extension('mykmeanssp', sources=['kmeans.c'])])
